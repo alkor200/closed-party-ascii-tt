806fa419807e9437a541453a1a970a6791013d29
Felix Nirsberger <felix@nirsberger.de>
2022-06-10 20:36:11 +0200

    changes

--- ascii-ctrl.py
+++ ascii-ctrl.py
@@ -3,7 +3,7 @@ import random
 
 if __name__ == '__main__':
 	while True:
-		with open('timetable-V2.txt', 'r') as file:
+		with open('timetable-v2.txt', 'r') as file:
 			for line in file.readlines():
 				print(line, end = '')
 				time.sleep(0.2)
